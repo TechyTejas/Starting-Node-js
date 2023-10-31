1a76ab07ebdbebd28d3fcd21e7add9a0b4351c6f
tejas <sadadetej.27@gmail.com>
2023-10-31 21:32:29 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const http = require('http')
+const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log("My First Server Code");
-})
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
 
+  console.log(`URL: ${url}, Method: ${method}`);
 
-server.listen(4000)
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body>');
+
+  // Check the URL and return custom responses
+  if (url === '/home' && method === 'GET') {
+    res.write('<h1>Welcome home</h1>');
+  } else if (url === '/about' && method === 'GET') {
+    res.write('<h1>Welcome to About Us page</h1>');
+  } else if (url === '/node' && method === 'GET') {
+    res.write('<h1>Welcome to my Node.js project</h1>');
+  } else {
+    res.write('<h1>Page not found</h1>');
+  }
+
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(4000);
